7a32cb86a60f318827bfa272d0f4d367b7bfda95
Jacob Packert <jacob.packert@mediacom.dk>
2017-01-15 11:15:48 +0100

    added button to trigger the script

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-$(function() {
+wodCreation = function() {
     $.ajax({
         url: 'https://spreadsheets.google.com/feeds/list/1hwe64eKPIU7W-jvzFWR8MUzW9DrQerp67G7QBVxvdjk/od6/public/values?alt=json',
         dataType: 'jsonp',
@@ -9,14 +9,15 @@ $(function() {
             wodname = data.gsx$name.$t;
             description = data.gsx$description.$t;
             if (data.gsx$score.$t == 0) {
-                    oldscore = "nothing. You haven't done this one before!";
-                } else {
-                    oldscore = data.gsx$score.$t
-                }
+                oldscore = "nothing. You haven't done this one before!";
+            } else {
+                oldscore = data.gsx$score.$t
+            }
             $('.wodname').html(wodname);
             $('.description').html(description);
             $('.oldscore').html(oldscore);
-
         }
     });
-});
+};
+
+$('.newwod').on('click', wodCreation);
