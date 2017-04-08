f1ddde0be6de0cc433b5d4be3d1046d4144c0779
Jacob Packert <jacob.packert@mediacom.dk>
2017-04-08 15:20:07 +0200

    some styling to make it all look better

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-wodCreation = function() {
+$(document).ready(function(){
+  wodCreation = function() {
     $.ajax({
         url: 'https://spreadsheets.google.com/feeds/list/1hwe64eKPIU7W-jvzFWR8MUzW9DrQerp67G7QBVxvdjk/od6/public/values?alt=json',
         dataType: 'jsonp',
         success: function(response) {
+        //   $.each(response.feed.entry, function(index, item) {
+        //   if(item.gsx$score.$t > 1){
+        //     console.log(item.gsx$name.$t + " scored " + item.gsx$score.$t);
+        //   };
+        // });
             console.log(response.feed.entry[0].gsx$description.$t);
             var wodNumber = Math.floor(response.feed.entry.length * Math.random());
             var data = response.feed.entry[wodNumber];
@@ -16,8 +22,12 @@ wodCreation = function() {
             $('.wodname').html(wodname);
             $('.description').html(description);
             $('.oldscore').html(oldscore);
+            $('.descriptiontexts').show();
+            $('.newwod').html("Another WOD 💪");
         }
     });
 };
 
 $('.newwod').on('click', wodCreation);
+
+});
